20b609534f77768bf980089dffc5bfcb17b57323
Flo535 <te@pelda.com>
2026-03-26 16:50:51 +0100

    Initial commit: NUnit tests only

---
+++ IzKalauzWPF.Tests/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using Izkalauz_WPF.Models;
+using System.Collections.Generic;
+using System.Runtime.Versioning; // <--- EZ KELL A FEJLÉCBE!
+
+namespace IzKalauzWPF.Tests
+{
+    [TestFixture]
+    [SupportedOSPlatform("windows")] // <--- IDE RAKD AZ OSZTÁLY FÖLÉ!
+    public class RecipeModelTests
+    {
+        [Test]
+        public void UjRecept_AlapertelmezettLista_NemNull()
+        {
+            var recept = new Recipe();
+            var hozzavalok = recept.Ingredients;
+            Assert.That(hozzavalok, Is.Not.Null);
+        }
+
+        [Test]
+        public void ReceptAdatok_HelyesenMentodnek()
+        {
+            var recept = new Recipe
+            {
+                Name = "Teszt Pörkölt",
+                Category = "Ebéd"
+            };
+
+            Assert.That(recept.Name, Is.EqualTo("Teszt Pörkölt"));
+            Assert.That(recept.Category, Is.EqualTo("Ebéd"));
+        }
+    }
+}
