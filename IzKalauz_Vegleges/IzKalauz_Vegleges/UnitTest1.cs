cba23d08f26c7bc39c74c6e4975a46c0d2310772
Flo535 <te@pelda.com>
2026-03-30 12:05:34 +0200

    fix: final source and solution files added, skipping UI files

---
+++ IzKalauz_Vegleges/IzKalauz_Vegleges/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace IzKalauz_Vegleges
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
